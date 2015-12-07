4aabbdff964f31780ecd1aa853bee6284bc7e08e
Lee Shiung <fenxls@hotmail.com>
2015-12-06 23:12:25 -0800

    test basic nn

--- networks/autoencoder.py
+++ networks/autoencoder.py
@@ -23,227 +23,204 @@ import math
 import numpy as np
 import os
 import random
-from six.moves import urllib
-from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
-import zipfile
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
-# Step 1: Download the data.
-url = 'http://mattmahoney.net/dc/'
+import input_data
+import BasicANN as BasicANN
 
-def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urllib.request.urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified', filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-        'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+# Basic model parameters as external flags.
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
 
-filename = maybe_download('text8.zip', 31344016)
+flags.DEFINE_integer('inputDim', 28*28, 'Input dimension') # 28*28 for BasicANN example
+flags.DEFINE_integer('num_classes', 10, 'Number of output classes (dimension)')
 
+flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
 
-# Read the data into a string.
-def read_data(filename):
-  f = zipfile.ZipFile(filename)
-  for name in f.namelist():
-    return f.read(name).split()
-  f.close()
+flags.DEFINE_integer('batch_size', 10, 'Batch size.  '
+                     'Must divide evenly into the dataset sizes.')
 
-words = read_data(filename)
-print('Data size', len(words))
+flags.DEFINE_string('train_dir', 'data', 'Directory to put the training data.')
+flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
+                     'for unit testing.')
 
-# Step 2: Build the dictionary and replace rare words with UNK token.
-vocabulary_size = 50000
+def placeholder_inputs(batch_size):
+  """Generate placeholder variables to represent the the input tensors.
 
-def build_dataset(words):
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)
-  data = list()
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count = unk_count + 1
-    data.append(index)
-  count[0][1] = unk_count
-  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-  return data, count, dictionary, reverse_dictionary
+  These placeholders are used as inputs by the rest of the model building
+  code and will be fed from the downloaded data in the .run() loop, below.
 
-data, count, dictionary, reverse_dictionary = build_dataset(words)
-del words  # Hint to reduce memory.
-print('Most common words (+UNK)', count[:5])
-print('Sample data', data[:10])
+  Args:
+    batch_size: The batch size will be baked into both placeholders.
 
-data_index = 0
+  Returns:
+    input_placeholder: Inputs placeholder.
+    labels_placeholder: Labels placeholder.
+  """
+  # Note that the shapes of the placeholders match the shapes of the full
+  # image and label tensors, except the first dimension is now batch_size
+  # rather than the full size of the train or test data sets.
+  input_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
+                                                         FLAGS.inputDim))
+  labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size))
+  return input_placeholder, labels_placeholder
 
+def fill_feed_dict(data_set, inputs_pl, labels_pl):
+  """Fills the feed_dict for training the given step.
 
-# Step 4: Function to generate a training batch for the skip-gram model.
-def generate_batch(batch_size, num_skips, skip_window):
-  global data_index
-  assert batch_size % num_skips == 0
-  assert num_skips <= 2 * skip_window
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1 # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  for _ in range(span):
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [ skip_window ]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target)
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  return batch, labels
+  A feed_dict takes the form of:
+  feed_dict = {
+      <placeholder>: <tensor of values to be passed for placeholder>,
+      ....
+  }
 
-batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
-for i in range(8):
-  print(batch[i], '->', labels[i, 0])
-  print(reverse_dictionary[batch[i]], '->', reverse_dictionary[labels[i, 0]])
+  Args:
+    data_set: The set of inputs and labels, from input_data.read_data_sets()
+    inputs_pl: The inputs placeholder, from placeholder_inputs().
+    labels_pl: The labels placeholder, from placeholder_inputs().
 
-# Step 5: Build and train a skip-gram model.
+  Returns:
+    feed_dict: The feed dictionary mapping from placeholders to values.
+  """
+  # Create the feed_dict for the placeholders filled with the next
+  # `batch size ` examples.
+  inputs_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
+                                                 FLAGS.fake_data)
+  feed_dict = {
+      inputs_pl: inputs_feed,
+      labels_pl: labels_feed,
+  }
+  return feed_dict
 
-batch_size = 128
-embedding_size = 128  # Dimension of the embedding vector.
-skip_window = 1       # How many words to consider left and right.
-num_skips = 2         # How many times to reuse an input to generate a label.
+def do_eval(sess,
+            eval_correct,
+            inputs_placeholder,
+            labels_placeholder,
+            data_set):
+  """Runs one evaluation against the full epoch of data.
 
-# We pick a random validation set to sample nearest neighbors. Here we limit the
-# validation samples to the words that have a low numeric ID, which by
-# construction are also the most frequent.
-valid_size = 16     # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
-valid_examples = np.array(random.sample(np.arange(valid_window), valid_size))
-num_sampled = 64    # Number of negative examples to sample.
+  Args:
+    sess: The session in which the model has been trained.
+    eval_correct: The Tensor that returns the number of correct predictions.
+    inputs_placeholder: The inputs placeholder.
+    labels_placeholder: The labels placeholder.
+    data_set: The set of inputs and labels to evaluate, from
+      input_data.read_data_sets().
+  """
+  # And run one epoch of eval.
+  true_count = 0  # Counts the number of correct predictions.
+  steps_per_epoch = data_set.num_examples // FLAGS.batch_size
+  num_examples = steps_per_epoch * FLAGS.batch_size
+  for step in xrange(steps_per_epoch):
+    feed_dict = fill_feed_dict(data_set,
+                               inputs_placeholder,
+                               labels_placeholder)
+    true_count += sess.run(eval_correct, feed_dict=feed_dict)
+  precision = true_count / num_examples
+  print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+        (num_examples, true_count, precision))
 
-graph = tf.Graph()
+def run_training():
+  """Train classifier for a number of steps."""
+  # Get the sets of inputs and labels for training, validation
+  data_sets = input_data.read_data_sets(FLAGS.train_dir, FLAGS.fake_data)
 
-with graph.as_default():
-  # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+  # Tell TensorFlow that the model will be built into the default Graph.
+  with tf.Graph().as_default():
+    # Generate placeholders for the inputs and labels.
+    inputs_placeholder, labels_placeholder = placeholder_inputs(
+        FLAGS.batch_size)
 
-  # Construct the variables.
-  embeddings = tf.Variable(
-      tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0), name='embeddings')
-  nce_weights = tf.Variable(
-      tf.truncated_normal([vocabulary_size, embedding_size],
-                          stddev=1.0 / math.sqrt(embedding_size)) , name='nce_weights')
-  nce_biases = tf.Variable(tf.zeros([vocabulary_size]), name='nce_biases')
+    # Build a Graph that computes predictions from the inference model.
+    logits = BasicANN.inference(inputs_placeholder,
+                             FLAGS.hidden1,
+                             FLAGS.hidden2)
 
-  # Connect Saver
-  saver = tf.train.Saver({embeddings, nce_weights, nce_biases},max_to_keep=2);
-  # Default saves ll variables
-  # saver = tf.train.Saver() 
+    # Add to the Graph the Ops for loss calculation.
+    loss = BasicANN.loss(logits, labels_placeholder)
 
-  # Look up embeddings for inputs.
-  embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+    # Add to the Graph the Ops that calculate and apply gradients.
+    train_op = BasicANN.training(loss, FLAGS.learning_rate)
 
-  # Compute the average NCE loss for the batch.
-  # tf.nce_loss automatically draws a new sample of the negative labels each
-  # time we evaluate the loss.
-  loss = tf.reduce_mean(
-      tf.nn.nce_loss(nce_weights, nce_biases, embed, train_labels,
-                     num_sampled, vocabulary_size))
+    # Add the Op to compare the logits to the labels during evaluation.
+    eval_correct = BasicANN.evaluation(logits, labels_placeholder)
 
-  # Construct the SGD optimizer using a learning rate of 1.0.
-  optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.merge_all_summaries()
 
-  # Compute the cosine similarity between minibatch examples and all embeddings.
-  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-  normalized_embeddings = embeddings / norm
-  valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-  similarity = tf.matmul(
-      valid_embeddings, normalized_embeddings, transpose_b=True)
+    # Create a saver for writing training checkpoints.
+    saver = tf.train.Saver()
 
-# Step 6: Begin training
-num_steps = 10001
+    # Create a session for running Ops on the Graph.
+    sess = tf.Session()
 
-with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  tf.initialize_all_variables().run()
-  print("Initialized")
+    # Run the Op to initialize the variables.
+    init = tf.initialize_all_variables()
+    sess.run(init)
 
-  average_loss = 0
-  for step in xrange(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs : batch_inputs, train_labels : batch_labels}
+    # Instantiate a SummaryWriter to output summaries and the Graph.
+    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+                                            graph_def=sess.graph_def)
 
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
+    # And then after everything is built, start the training loop.
+    for step in xrange(FLAGS.max_steps):
+      start_time = time.time()
 
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss = average_loss / 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print("Average loss at step ", step, ": ", average_loss)
-      average_loss = 0
+      # Fill a feed dictionary with the actual set of inputs and labels
+      # for this particular training step.
+      feed_dict = fill_feed_dict(data_sets.train,
+                                 inputs_placeholder,
+                                 labels_placeholder)
 
-    # note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in xrange(valid_size):
-        valid_word = reverse_dictionary[valid_examples[i]]
-        top_k = 8 # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k+1]
-        log_str = "Nearest to %s:" % valid_word
-        for k in xrange(top_k):
-          close_word = reverse_dictionary[nearest[k]]
-          log_str = "%s %s," % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
-  # save the checkpoint
-  saver.save(session, 'embedding_checkpoint.ckpt', global_step=step, latest_filename='final_embedding.ckpt')
+      # Run one step of the model.  The return values are the activations
+      # from the `train_op` (which is discarded) and the `loss` Op.  To
+      # inspect the values of your Ops or variables, you may include them
+      # in the list passed to sess.run() and the value tensors will be
+      # returned in the tuple from the call.
+      _, loss_value = sess.run([train_op, loss],
+                               feed_dict=feed_dict)
 
-# Step 7: Visualize the embeddings.
+      duration = time.time() - start_time
 
-def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-  plt.figure(figsize=(18, 18))  #in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i,:]
-    plt.scatter(x, y)
-    plt.annotate(label,
-                 xy=(x, y),
-                 xytext=(5, 2),
-                 textcoords='offset points',
-                 ha='right',
-                 va='bottom')
+      # Write the summaries and print an overview fairly often.
+      if step % 100 == 0:
+        # Print status to stdout.
+        print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+        # Update the events file.
+        summary_str = sess.run(summary_op, feed_dict=feed_dict)
+        summary_writer.add_summary(summary_str, step)
 
-  plt.savefig(filename)
+      # Save a checkpoint and evaluate the model periodically.
+      if (step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+        saver.save(sess, FLAGS.train_dir, global_step=step)
+        # Evaluate against the training set.
+        print('Training Data Eval:')
+        do_eval(sess,
+                eval_correct,
+                inputs_placeholder,
+                labels_placeholder,
+                data_sets.train)
+        # Evaluate against the validation set.
+        print('Validation Data Eval:')
+        do_eval(sess,
+                eval_correct,
+                inputs_placeholder,
+                labels_placeholder,
+                data_sets.validation)
+        # Evaluate against the test set.
+        print('Test Data Eval:')
+        do_eval(sess,
+                eval_correct,
+                inputs_placeholder,
+                labels_placeholder,
+                data_sets.test)
 
-try:
-  from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
+def main(_):
+  run_training()
 
-  tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-  plot_only = 500
-  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only,:])
-  labels = [reverse_dictionary[i] for i in xrange(plot_only)]
-  plot_with_labels(low_dim_embs, labels)
-
-except ImportError:
-  print(ImportError, "Please install sklearn and matplotlib to visualize embeddings.")
-
-
+if __name__ == '__main__':
+  tf.app.run()
