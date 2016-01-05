ba34cfd4ab92550901ac34f1fb29bcd07f961b6a
Lee Shiung <fenxls@hotmail.com>
2016-01-04 23:44:12 -0800

    create test data sets

--- kaggle/airbnb/script/input_data.py
+++ kaggle/airbnb/script/input_data.py
@@ -103,6 +103,64 @@ class UserDataSet(object):
     end = self._index_in_epoch
     return self._input[start:end], self._output[start:end]
 
+class UserDataTestSet(object):
+  def __init__(self, train_users, train_users_header, one_hot=False):
+    self._train_users = train_users
+    self._train_users_header = train_users_header
+
+    self._num_examples = train_users.shape[0]
+
+    
+    # TODO import the look up table and nomalization scale
+    self.train_users_str2ind, self.train_users_ind2str = input_util.tokenizeByColumn(train_users,[4,6,8,9,10,11,12,13,14])
+
+
+
+    input_util.convertToNum(train_users, [2,5,7], default = 0.0)
+    input_util.convertDate(train_users, [1,3], baseDateStr = '1/1/2010', default = -1)
+    input_util.columnNormalizer(train_users, [1,3], minVal = 0.0, maxVal = 10.0)
+    input_util.columnNormalizer(train_users, [2,5,7], minVal = 0.0, maxVal = 10.0)
+
+    self._input = train_users[:, range(1,14)].astype(float)
+
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def input(self):
+    return self._input
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size):
+    """Return the next `batch_size` examples from this data set."""
+    
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      
+      # Shuffle the data
+      perm = np.arange(self._num_examples)
+      np.random.shuffle(perm)
+
+      self._input = self._input[perm]
+
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+
+    end = self._index_in_epoch
+    return self._input[start:end]
+
 def read_data_sets():
   class DataSets(object):
     pass
