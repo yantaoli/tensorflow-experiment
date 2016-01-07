acc7c9dcc7347384109c607fd205ff757db6e639
Lee Shiung <fenxls@hotmail.com>
2016-01-07 01:31:55 -0800

    ready for testing

--- kaggle/airbnb/script/input_data.py
+++ kaggle/airbnb/script/input_data.py
@@ -42,23 +42,46 @@ def extract_csv(filename, hasHeader = True):
         data.append(row)
     return np.array(data), header
 
-# TODO support test datasets
+# support test datasets
 class UserDataSet(object):
-  def __init__(self, train_users, train_users_header, test_set, test_set_header,one_hot=False):
-    self._train_users = train_users
-    self._train_users_header = train_users_header
+  def __init__(self, training_set, training_set_header, test_set, test_set_header):
+    self._training_set = training_set
+    self._training_set_header = training_set_header
+    self._test_set = test_set
 
-    self._num_examples = train_users.shape[0]
+    self._num_examples = training_set.shape[0]
+    self._total_dim = training_set.shape[1]
+    self._num_tests = test_set.shape[0]
+    self._test_total_dim = test_set.shape[1]
 
-    self.train_users_str2ind, self.train_users_ind2str = input_util.tokenizeByColumn(train_users,[4,6,8,9,10,11,12,13,14,15])
-    input_util.convertToNum(train_users, [2,5,7], default = 0.0)
-    input_util.convertDate(train_users, [1,3], baseDateStr = '1/1/2010', default = -1)
-    input_util.columnNormalizer(train_users, [1,3], minVal = 0.0, maxVal = 10.0)
-    input_util.columnNormalizer(train_users, [2,5,7], minVal = 0.0, maxVal = 10.0)
+    # Define set parameters
+    self._input_dim = 15 # column 0 to 14
+    self._output_dim = 1
+    self._null_value = -99
 
+    # Augment test set to the same size as training set
+    assert self._test_total_dim <= self._total_dim
+    if self._test_total_dim < self._total_dim:
+      # resources at: http://stackoverflow.com/questions/8486294/how-to-add-an-extra-column-to-an-numpy-array
+      numColumnToAppend = self._total_dim - self._test_total_dim
+      augmentedTestSet = np.c_[test_set, np.ones(self._num_tests,numColumnToAppend) * self._null_value]
+    else:
+      # to assert test dim should never be 
+      augmentedTestSet = test_set
 
-    self._input = train_users[:, range(1,15)].astype(float)
-    self._output = train_users[:, 15].astype(int)
+    # Augment the sets
+    self._augmentedSet = np.r_[self._training_set ,augmentedTestSet]
+
+    # tokenize + normalize
+    self.str2ind, self.ind2str = input_util.tokenizeByColumn(self._augmentedSet,[4,6,8,9,10,11,12,13,14,15])
+    input_util.convertToNum(self._augmentedSet, [2,5,7], default = 0.0)
+    input_util.convertDate(self._augmentedSet, [1,3], baseDateStr = '1/1/2010', default = -1)
+    input_util.columnNormalizer(self._augmentedSet, [1,3], minVal = 0.0, maxVal = 10.0)
+    input_util.columnNormalizer(self._augmentedSet, [2,5,7], minVal = 0.0, maxVal = 10.0)
+
+    self._input = self._augmentedSet[range(0,self._num_examples), range(1,15)].astype(float)
+    self._output = self._augmentedSet[range(0,self._num_examples), 15].astype(int)
+    self._testInput = self._augmentedSet[range(self._num_examples, self._num_examples + self._num_tests), range(1,15)].astype(float)
 
     self._epochs_completed = 0
     self._index_in_epoch = 0
@@ -70,6 +93,10 @@ class UserDataSet(object):
   @property
   def output(self):
     return self._output
+
+  @property
+  def testInput(self):
+    return self._testInput
 
   @property
   def num_examples(self):
@@ -103,79 +130,27 @@ class UserDataSet(object):
     end = self._index_in_epoch
     return self._input[start:end], self._output[start:end]
 
-class UserDataTestSet(object):
-  def __init__(self, train_users, train_users_header, one_hot=False):
-    self._train_users = train_users
-    self._train_users_header = train_users_header
 
-    self._num_examples = train_users.shape[0]
-
-    
-    # TODO import the look up table and nomalization scale
-    self.train_users_str2ind, self.train_users_ind2str = input_util.tokenizeByColumn(train_users,[4,6,8,9,10,11,12,13,14])
-
-
-
-    input_util.convertToNum(train_users, [2,5,7], default = 0.0)
-    input_util.convertDate(train_users, [1,3], baseDateStr = '1/1/2010', default = -1)
-    input_util.columnNormalizer(train_users, [1,3], minVal = 0.0, maxVal = 10.0)
-    input_util.columnNormalizer(train_users, [2,5,7], minVal = 0.0, maxVal = 10.0)
-
-    self._input = train_users[:, range(1,14)].astype(float)
-
-    self._epochs_completed = 0
-    self._index_in_epoch = 0
-
-  @property
-  def input(self):
-    return self._input
-
-  @property
-  def num_examples(self):
-    return self._num_examples
-
-  @property
-  def epochs_completed(self):
-    return self._epochs_completed
-
-  def next_batch(self, batch_size):
-    """Return the next `batch_size` examples from this data set."""
-    
-    start = self._index_in_epoch
-    self._index_in_epoch += batch_size
-    if self._index_in_epoch > self._num_examples:
-      # Finished epoch
-      self._epochs_completed += 1
-      
-      # Shuffle the data
-      perm = np.arange(self._num_examples)
-      np.random.shuffle(perm)
-
-      self._input = self._input[perm]
-
-      # Start next epoch
-      start = 0
-      self._index_in_epoch = batch_size
-      assert batch_size <= self._num_examples
-
-    end = self._index_in_epoch
-    return self._input[start:end]
 
 def read_data_sets():
   class DataSets(object):
     pass
   data_sets = DataSets()
 
-  TRAIN_USERS = 'train_users.csv'
+  training_set = 'training_set.csv'
   TEST_USERS = 'test_users.csv'
   #SESSIONS = 'sessions.csv' # comment out because sessions data is too big to fit in python memory
   COUNTRIES = 'countries.csv'
   AGE_GENDER_BKTS = 'age_gender_bkts.csv'
 
-  train_users, train_header= extract_csv(TRAIN_USERS)
+  training_set, train_header= extract_csv(training_set)
   test_users, test_headers = extract_csv(TEST_USERS)
+
+  # TODO append test users after train users then perform normalizations
+
+
   # process training data
-  data_sets.trainData = UserDataSet(train_users, train_header, test_users, test_headers)
+  data_sets.trainData = UserDataSet(training_set, train_header, test_users, test_headers)
 
 
   # data_sets.sessions, _ = extract_csv(SESSIONS)
